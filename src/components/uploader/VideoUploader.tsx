75b783b752c9cc2df56c80ce6b29c04d97c74102
Meet Makwana <meetmakwana203@gmail.com>
2024-06-26 15:14:17 +0530

    feat: added response delay and response area component

--- src/components/uploader/VideoUploader.tsx
+++ src/components/uploader/VideoUploader.tsx
@@ -5,10 +5,14 @@ const VideoUploader = () => {
     const [files, setFiles] = useState<any>([]); // Add type annotation to the state
     const [response, setResponse] = useState("");
     const [loading, setLoading] = useState(0);
+
+    const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
     console.log("resp---- ", response);
 
     const dummyCall = async () => {
         try {
+            setResponse("");
             setLoading(30);
             const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
                 method: 'POST',
@@ -22,15 +26,21 @@ const VideoUploader = () => {
                 },
             })
             setLoading(40);
+            await delay(1000); // 1 second delay
 
             if (!response.ok) {
                 throw new Error(`HTTP error! Status: ${response.status}`);
             }
             setLoading(70);
+            await delay(1000); // 1 second delay
+
             const responseData = await response.json();
-            setResponse(responseData.title + " " + responseData.body);
+            // setResponse(responseData.title + " " + responseData.body);
+            setResponse(JSON.stringify(responseData).repeat(100));
             console.log("Successfully posted : ", responseData);
             setLoading(100);
+            await delay(1000); // 1 second delay
+            setLoading(101);
         }
         catch (error) {
             setLoading(100);
@@ -69,12 +79,12 @@ const VideoUploader = () => {
     }
     return (
         <div className='video-uploader-wrapper'>
-            <h1>Upload Files</h1>
+            <h1 className="text-4xl font-bold mb-4">Upload Files</h1>
 
             {/* <input type="file" multiple onChange={(e) => setFiles(e.target.files)} accept=".mov,.mp4" /> */}
             <input
                 type="file"
-                className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+                className="file-input file-input-bordered file-input-primary w-full max-w-xs glass"
                 multiple
                 onChange={(e) => setFiles(e.target.files)}
                 accept=".mov,.mp4"
@@ -83,13 +93,20 @@ const VideoUploader = () => {
             {/* <button onClick={handleUpload}>Upload</button> */}
             <button className="btn btn-outline btn-primary" onClick={handleUpload}>Upload</button>
 
-            {loading > 0 && loading !== 100 && (
-                <progress className="progress w-70" value={loading} max="100"></progress>
-            )}
+            <div className="progress-wrapper response-area">
+                {loading > 0 && loading !== 101 && (
+                    <progress className="progress" value={loading} max="100"></progress>
+                )}
+                {response.length > 0  && response}
+            </div>
 
-            {response.length > 0 && (
-                <textarea className="textarea textarea-primary">{response}</textarea>
-            )}
+            {/* {response.length > 0 && (
+                <>
+                <div className="response-area">
+                    {response}
+                </div>
+                </>
+            )} */}
         </div>
     )
 }
