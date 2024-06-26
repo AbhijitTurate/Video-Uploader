0f9a8fd027687989aea6d0e948c41256bd2c2322
Meet Makwana <meetmakwana203@gmail.com>
2024-06-26 15:44:59 +0530

    feat: optimized theme generation using themes array

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,39 @@
 import React from 'react'
 
+const themes = [
+    "default",
+    "light",
+    "synthwave",
+    // "cupcake",
+    "bumblebee",
+    "emerald",
+    "cyberpunk",
+    "retro",
+    "valentine",
+    "aqua",
+    "halloween",
+    "garden",
+    "forest",
+    "lofi",
+    "pastel",
+    "fantasy",
+    "wireframe",
+    "black",
+    "luxury",
+    "dracula",
+    "cmyk",
+    "autumn",
+    "business",
+    "acid",
+    "lemonade",
+    "night",
+    "coffee",
+    "winter",
+    "dim",
+    "nord",
+    "sunset",
+];
+
 const Header = () => {
     return (
         <div className="navbar bg-base-100">
@@ -16,56 +50,23 @@ const Header = () => {
                             <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
                         </svg>
                     </div>
-                    <ul tabIndex={0} className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl">
-                        <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Default"
-                                value="default" />
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Retro"
-                                value="retro" />
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Synthwave"
-                                value="synthwave" />
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Cyberpunk"
-                                value="cyberpunk" />
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Valentine"
-                                value="valentine" />
-                        </li>
-                        <li>
-                            <input
-                                type="radio"
-                                name="theme-dropdown"
-                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
-                                aria-label="Aqua"
-                                value="aqua" />
-                        </li>
+                    <ul
+                        tabIndex={0}
+                        className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl"
+                    >
+                        {themes.map((theme) => (
+                            <li key={theme}>
+                                <input
+                                    type="radio"
+                                    name="theme-dropdown"
+                                    className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                                    aria-label={theme.charAt(0).toUpperCase() + theme.slice(1)}
+                                    value={theme}
+                                />
+                            </li>
+                        ))}
                     </ul>
+
                 </div>
             </div>
             <div className="navbar-center">
