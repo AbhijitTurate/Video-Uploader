99cde3bf516de5e482b3232e5228658d58fa1f2f
Meet Makwana <meetmakwana203@gmail.com>
2024-06-26 15:33:20 +0530

    feat: theme switching functionaliy

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -5,27 +5,67 @@ const Header = () => {
         <div className="navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
-                    {/* <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                    <div tabIndex={0} role="button" className="btn m-1">
+                        Theme
                         <svg
+                            width="12px"
+                            height="12px"
+                            className="inline-block h-2 w-2 fill-current opacity-60"
                             xmlns="http://www.w3.org/2000/svg"
-                            className="h-5 w-5"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            stroke="currentColor">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M4 6h16M4 12h16M4 18h7" />
+                            viewBox="0 0 2048 2048">
+                            <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
                         </svg>
                     </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><a>Homepage</a></li>
-                        <li><a>Portfolio</a></li>
-                        <li><a>About</a></li>
-                    </ul> */}
+                    <ul tabIndex={0} className="dropdown-content bg-base-300 rounded-box z-[1] w-52 p-2 shadow-2xl">
+                        <li>
+                            <input
+                                type="radio"
+                                name="theme-dropdown"
+                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                                aria-label="Default"
+                                value="default" />
+                        </li>
+                        <li>
+                            <input
+                                type="radio"
+                                name="theme-dropdown"
+                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                                aria-label="Retro"
+                                value="retro" />
+                        </li>
+                        <li>
+                            <input
+                                type="radio"
+                                name="theme-dropdown"
+                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                                aria-label="Synthwave"
+                                value="synthwave" />
+                        </li>
+                        <li>
+                            <input
+                                type="radio"
+                                name="theme-dropdown"
+                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                                aria-label="Cyberpunk"
+                                value="cyberpunk" />
+                        </li>
+                        <li>
+                            <input
+                                type="radio"
+                                name="theme-dropdown"
+                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                                aria-label="Valentine"
+                                value="valentine" />
+                        </li>
+                        <li>
+                            <input
+                                type="radio"
+                                name="theme-dropdown"
+                                className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+                                aria-label="Aqua"
+                                value="aqua" />
+                        </li>
+                    </ul>
                 </div>
             </div>
             <div className="navbar-center">
