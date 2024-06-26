9b724cf25835516a5b3bee55c09473d4c0390acd
Meet Makwana <meetmakwana203@gmail.com>
2024-06-26 11:34:32 +0530

    feat: header setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import './App.css'
 import VideoUploader from './VideoUploader'
+import Header from './components/header/Header'
 
 function App() {
   return (
     <>
+      <Header />
       <VideoUploader />
     </>
   )
